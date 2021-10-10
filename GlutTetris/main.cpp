67cb6a145714fba48b2075be33160e075a2337d3
Greshch <greshchook@gmail.com>
2021-10-10 20:28:24 +0300

    OpenGl carcas implemented

--- GlutTetris/main.cpp
+++ GlutTetris/main.cpp
@@ -1,8 +1,27 @@
 #include <GL/freeglut.h>
 #include <stdlib.h>
+#include "screen.h"
+
+void DisplayFuncCallback();
 
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+	glutInitWindowPosition(640, 16);
+	glutInitWindowSize(WIDTH, HEIGHT);
+	glutCreateWindow("Tetris");
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, WIDTH, 0.0, HEIGHT, -1.0, 1.0);
+	glutDisplayFunc(DisplayFuncCallback);
+	glutMainLoop();
 	return 0;
+}
+
+void DisplayFuncCallback()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
 }
--- GlutTetris/screen.h
+++ GlutTetris/screen.h
@@ -2,6 +2,6 @@
 
 int const M = 20;
 int const N = 10;
-int const SCALE = 30;
+int const SCALE = 28;
 int const WIDTH = (N + 2 * N / 3) * SCALE;
 int const HEIGHT = (M + M / 2) * SCALE;
