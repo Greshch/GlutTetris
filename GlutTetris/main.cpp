6706d6fdf5f3c3da6c3dd9bc2dd29d322605b5a8
Greshch <greshchook@gmail.com>
2021-10-10 20:03:43 +0300

    first

---
+++ GlutTetris/main.cpp
@@ -0,0 +1,8 @@
+#include <GL/freeglut.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	return 0;
+}
---
+++ GlutTetris/screen.h
@@ -0,0 +1,7 @@
+#pragma once
+
+int const M = 20;
+int const N = 10;
+int const SCALE = 30;
+int const WIDTH = (N + 2 * N / 3) * SCALE;
+int const HEIGHT = (M + M / 2) * SCALE;
