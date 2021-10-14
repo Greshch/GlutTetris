ea5d6efc64042ad7c81bfdeb9f0edb207e2e127e
Greshch <greshchook@gmail.com>
2021-10-14 10:10:44 +0300

    tetris 2 draw window opengl

--- GlutTetris/main.cpp
+++ GlutTetris/main.cpp
@@ -23,7 +23,20 @@ void Keyboard(int key, int, int);
 
 int main(int argc, char** argv)
 {
-	
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowPosition(600, 32);
+	glutInitWindowSize(WIDTH, HEIGHT);
+	glutCreateWindow("Tetris Game");
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, COL, ROW, 0.0, -1.0, 1.0);
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+
+	glutDisplayFunc(Renderer);
+	glutTimerFunc(pause, Timer, 0);
+	glutSpecialFunc(Keyboard);
+	glutMainLoop();
 	return 0;
 }
 
