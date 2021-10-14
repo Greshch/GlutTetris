b6caeff34a2bd7495e78f2ac167bf6a9edcdce4f
Greshch <greshchook@gmail.com>
2021-10-14 11:38:52 +0300

    tetris 4 define tetramino and implemented SetTetramino

--- GlutTetris/tetromino.cpp
+++ GlutTetris/tetromino.cpp
@@ -1,2 +1,11 @@
 #include "tetromino.h"
 #include "global_consts.h"
+
+void SetTetramino(Tetramino& obj, int id, int left, int up)
+{
+	for (size_t i = 0; i < TETRAMINO_SZ; i++)
+	{
+		obj[i].x = tetraminos[id][i] % 2 + left;
+		obj[i].y = tetraminos[id][i] / 2 + up;
+	}
+}
--- GlutTetris/tetromino.h
+++ GlutTetris/tetromino.h
@@ -1,2 +1,28 @@
 #pragma once
+#include "global_consts.h"
+
+struct Point
+{
+	int x;
+	int y;
+};
+
+int const TETRAMINO_SZ = 4;
+int const TETRAMINO_CNT = 7;
+
+typedef Point Tetramino[TETRAMINO_SZ];
+
+int const const tetraminos[TETRAMINO_CNT][TETRAMINO_SZ]
+{
+	{2, 4, 5, 6}, //T
+	{2, 4, 5, 7}, //S
+	{3, 5, 4, 6}, //Z
+	{2, 3, 5, 4}, //O
+	{1, 3, 5, 7}, //I
+	{3, 5, 7, 6}, //J
+	{2, 4, 6, 7}  //L
+};
+
+void SetTetramino(Tetramino& obj, int id, int left = COL / 2, int up = FIELD_UP);
+
 
